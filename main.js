419ed28eedeaca67d5d8130616a35d4b5f5791a6
Road2Full-Stack <haldanedsa@gmail.com>
2021-11-23 17:34:27 +0000

    Add progress to project

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-// min 130
+// min 140
 
-
+const handOptions = {
+    "rock": "/images/Rock.png",
+    "paper": "/images/Paper.png",
+    "scissors": "/images/Scissors.png"
+} 
 
 const pickUserHand = (hand) => {
+    console.log(hand);
     let hands = document.querySelector('.hands');
     hands.style.display = 'none';
-    console.log(hand);
 }
+
+let contest = document.querySelector('.contest');
+contest.style.display = "flex";
+
+// set user pick
+
+document.getElementById("userPickImage").src = handOptions(hand);
