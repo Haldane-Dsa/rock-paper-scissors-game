4e78825d701949fe45989c65d0c28325fc2ef5dc
Road2Full-Stack <haldanedsa@gmail.com>
2021-11-29 19:08:29 +0000

    Add progress

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 140 minutes
+// 156 minutes
 
 const handOptions = {
     "rock": "/images/Rock.png",
@@ -13,8 +13,23 @@ const pickUserHand = (hand) => {
 
     // show next page with hand you picked
     let contest = document.querySelector(".contest");
-contest.style.display = "flex";
+    contest.style.display = "flex";
 
     // set user pick
     document.getElementById("userPickImage").src = handOptions[hand];
+
+    let cpHand = pickComputerHand();
+}
+
+const pickComputerHand = () => {
+    let hands = ["rock", "paper", "scissors"];
+    let cpHand = hands[Math.floor(Math.random() * 3)];
+
+    // set computer pick
+    document.getElementById("computerPickImage").src = handOptions[cpHand];
+    return cpHand;
+}
+
+const referee = (userHand, cpHand) => {
+
 }
